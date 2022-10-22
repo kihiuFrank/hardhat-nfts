93d6cb827539280b370c8e2d6569635dc799e628
kihiuFrank <franklinekihiu@gmail.com>
2022-10-22 07:40:17 +0300

    deploying basicNft

--- contracts/BasicNft.sol
+++ contracts/BasicNft.sol
@@ -4,5 +4,32 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
-    constructor() ERC721("Dogie", "DOG") {}
+    string public constant TOKEN_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("Dogie", "DOG") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft() public returns (uint256) {
+        // mint the function to whoever calls it (msg.sender)
+        // then give it a tokenId
+        _safeMint(msg.sender, s_tokenCounter);
+        //anytime we mint an NFT we up/add the tokenCounter
+        s_tokenCounter = s_tokenCounter + 1;
+
+        return s_tokenCounter;
+    }
+
+    function tokenURI(
+        uint256 /*tokenId */
+    ) public view override returns (string memory) {
+        // require(_exists(tokenId))
+        return TOKEN_URI;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
 }
