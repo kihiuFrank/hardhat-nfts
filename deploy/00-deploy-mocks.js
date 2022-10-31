5704c86f6b342361812fa786410d2bde5153cd5b
kihiuFrank <franklinekihiu@gmail.com>
2022-10-31 12:29:30 +0300

    uploading to pinata

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,25 +1,28 @@
+const { ethers, network } = require("hardhat")
 const { developmentChains } = require("../helper-hardhat-config")
 
-module.exports = async ({ hre }) => {
-    const { deployments, getNamedAccounts, network, ethers } = hre
-    const BASE_FEE = ethers.utils.parseEther("0.25") // 0.25 is this the premium in LINK?
-    const GAS_PRICE_LINK = 1e9 // link per gas, is this the gas lane? // 0.000000001 LINK per gas
+const BASE_FEE = ethers.utils.parseEther("0.25") //0.25 is the premium. It costs 0.25 LINK per request
+const GAS_PRICE_LINK = 1e9 // 1000000000 // calculated value based on the gas price of the chain. // or link per gas
 
+module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const args = [BASE_FEE, GAS_PRICE_LINK]
-    // If we are on a local development network, we need to deploy mocks!
+    //const chainId = network.config.chainId
+
     if (developmentChains.includes(network.name)) {
-        log("Local network detected! Deploying mocks...")
-        //deploy a mock vrfcoordinator
+        log("Local Network detected! Deploying mocks...")
+        //deploy a mock vrfCoordinator
         await deploy("VRFCoordinatorV2Mock", {
             from: deployer,
             log: true,
             args: args,
         })
-
         log("Mocks Deployed!")
-        log("---------------------------------------------------------------------------------")
+        log(
+            "---------------------------------------------------------------------------------------------------------------------"
+        )
     }
 }
+
 module.exports.tags = ["all", "mocks"]
--- deploy/02-deploy-randon-ipfs-nft.js
+++ deploy/02-deploy-randon-ipfs-nft.js
@@ -1,11 +1,24 @@
 const { network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
+const { storeImages } = require("../utils/uploadToPinata")
+
+const imageslocation = "./images/randomNft/"
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
+
+    let tokenUris
+    // get IPFS hashes of our images
+    if (process.env.UPLOAD_TO_PINATA == "true") {
+        tokenUris = await handleTokenUris()
+    }
+
+    //1. with our own IPFS Node. https://docs.ipfs.io/ (centralised since we are the only ones running the node)
+    //2. pinata https://www.pinata.cloud/ (centralised entity) - using this
+    //3. nft.storage https://nft.storage/ (Uses filecoin blockchain - decentralised)
 
     let vrfCoordinatorV2Address, subscriptionId
 
@@ -21,12 +34,23 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     }
 
     log("-------------------------------------------------------------------")
-    const args = [
+    await storeImages(imageslocation)
+    /* const args = [
         vrfCoordinatorV2Address,
         subscriptionId,
         networkConfig[chainId].gasLane,
         networkConfig[chainId].callbackGasLimit,
         //tokenUris
         networkConfig[chainId].mintFee,
-    ]
+    ] */
 }
+
+async function handleTokenUris() {
+    tokenUris = []
+    // we need to store the Image in IPFS
+    // store the metadata in IPFS
+
+    return tokenUris
+}
+
+module.exports.tags = ["all", "randomipfs", "main"]
