982b1889d2894ce722fc6186b4b2d80fab849755
kihiuFrank <franklinekihiu@gmail.com>
2022-11-05 16:01:47 +0300

    dynamic-svg-nft deployment

--- deploy/03-dynamic-svg-nft.js
+++ deploy/03-dynamic-svg-nft.js
@@ -1,6 +1,7 @@
 const { network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
+const fs = require("fs")
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
@@ -18,4 +19,31 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
         // when in testnet or mainnet
         ethUsdPriceFeedAddress = networkConfig[chainId].ethUsdPriceFeed
     }
+
+    log("-------------------------------------------------------------------")
+
+    // getting the lowSvg & highSvg
+    const lowSvg = fs.readFileSync("./images/dynamicNft/frown.svg", { encoding: "utf8" })
+    const highSvg = fs.readFileSync("./images/dynamicNft/happy.svg", { encoding: "utf8" })
+
+    // now we deploy
+    console.log("Deploying Contract...")
+    args = [ethUsdPriceFeedAddress, lowSvg, highSvg]
+    const dynamicSvgNft = await deploy("DynamicSvgNft", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+    console.log("Contract Deployed!")
+
+    log("-------------------------------------------------------------------")
+    // Verify the deployment
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying contract...")
+        await verify(dynamicSvgNft.address, args)
+    }
+    console.log("Verified!")
 }
+
+module.exports.tags = ["all", "dynamicsvg", "main"]
